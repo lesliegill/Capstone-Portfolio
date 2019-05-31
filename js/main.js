040f329226867d2d71642327f2805708728243d5
lesliegill <lesliegill10@live.com>
2019-05-30 18:06:42 -0700

    commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 
-const element =  document.querySelector('.my-example')
-element.classList.add('animated', 'bounceOutLeft')
-
-window.addEventListener('scroll', () => {
-
-
-});
-
-
-
 const scroll = new SmoothScroll('a[href*="#"]', {
     speed:800
 });
+
+
