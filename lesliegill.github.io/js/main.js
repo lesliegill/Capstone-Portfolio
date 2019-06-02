4cbbc231d847bfbedaca723121c512e2f23a5299
Leslie Gill <lesliegill10@live.com>
2019-06-01 23:16:18 -0700

    added js

---
+++ lesliegill.github.io/js/main.js
@@ -0,0 +1,47 @@
+ScrollOut({
+      onShown(el) {
+    el.classList.add("animated");
+  }
+    });
+   
+  window.sr = ScrollReveal();
+  sr.reveal('.developer', {
+    duration:3000,
+    origin:'right',
+    distance:'300px'
+  });
+   sr.reveal('.project1', {
+    duration:3000,
+    origin:'left',
+    distance:'300px'
+  });
+     sr.reveal('.project2', {
+    duration:3000,
+    origin:'right',
+    distance:'300px'
+
+  });
+     sr.reveal('.project3', {
+    duration:3000,
+    origin:'left',
+    distance:'300px'
+
+  });
+    sr.reveal('.project4', {
+    duration:3000,
+    origin:'right',
+    distance:'300px'
+
+  });
+     sr.reveal('.project5', {
+    duration:3000,
+    origin:'left',
+    distance:'300px'
+  });
+
+    sr.reveal('.project6', {
+    duration:3000,
+    origin:'right',
+    distance:'300px'
+  });
+  
